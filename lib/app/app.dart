027c0c5929d6647fc9af67a0ce38e14fc838e58e
TalgatBatyrov <90198155+TalgatBatyrov@users.noreply.github.com>
2022-07-28 21:30:55 +0600

    separated main and app

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../elements/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pomodoro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
