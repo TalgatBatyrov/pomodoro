b46036a24166c94342f0d5d1d9bacfaf4a1d4761
TalgatBatyrov <90198155+TalgatBatyrov@users.noreply.github.com>
2022-07-29 10:34:16 +0600

    feat: timer_cubit

---
+++ lib/cubits/timer_cubit.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pomodoro/cubits/timer_play_button_cubit.dart';
+import 'package:pomodoro/cubits/timer_state_cubit.dart';
+
+class TimerCubit extends Cubit<Duration> {
+  final TimerStateCubit _timerStateCubit;
+  final TimerPlayButtonCubit _timerPlayButtonCubit;
+
+  late StreamSubscription _timerStateSub;
+  late StreamSubscription _timerPlayButtonSub;
+  late Timer? _lastTimer;
+  TimerCubit(
+    this._timerStateCubit,
+    this._timerPlayButtonCubit,
+  ) : super(const Duration(seconds: 25)) {
+    _timerStateSub = _timerStateCubit.stream.listen(_onTimerStateChanged);
+    _timerPlayButtonSub =
+        _timerPlayButtonCubit.stream.listen(_onTimerPlayButtonStateChanged);
+  }
+
+  void _onTimerPlayButtonStateChanged(TimerPlayButtonState event) {
+    if (event == TimerPlayButtonState.play) {
+      _lastTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        final current = state - const Duration(seconds: 1);
+        emit(current);
+        final isFinished = current.inSeconds == 0;
+        if (isFinished) {
+          timer.cancel();
+          _timerStateCubit.nextPage();
+        }
+      });
+    }
+    if (event == TimerPlayButtonState.pause) {
+      _lastTimer.cancel();
+    }
+  }
+
+  void _onTimerStateChanged(TimerState event) {
+    if (event == TimerState.focus) {
+      emit(const Duration(seconds: 25));
+      return;
+    }
+    if (event == TimerState.longBreak) {
+      emit(const Duration(seconds: 15));
+      return;
+    }
+    if (event == TimerState.shortBreak) {
+      emit(const Duration(seconds: 5));
+      return;
+    }
+  }
+}
